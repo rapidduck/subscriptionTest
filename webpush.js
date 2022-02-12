16cb599439ee65a5de85328d4f86a4e9fb38d60e
rapidduck <robin.skaba@volny.cz>
2022-02-12 10:58:02 +0100

    new

--- webpush.js
+++ webpush.js
@@ -2,9 +2,8 @@ const webpush = require('web-push');
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
-  publicKey:
-    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
-  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+  publicKey: 'BFHeG_-cgzrK3xohv4poJ7z4tpX9oDROrj8Rj3fXCjpuuK8pKdNSmF5T2rKKEY0lammQ_2_f1t_NGyG0RqOuynk',
+  privateKey: '-RMX3o_klK67WLgFA2vc6IURRwqxkUQzDkHyh4bWAEw'
 };
 
 webpush.setVapidDetails(
@@ -14,6 +13,6 @@ webpush.setVapidDetails(
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/cmlINweO6rM:APA91bGplfl15u046NPQqDxDbLUgI0h5a69H7idJ73SqQ5T4RKEhAV1X_qo1pkEq5NdcPresq1QNqC_hRRADnEvV6Jfyllgwwn0drfqvVve14q1gBgKM9t37mbmt3uNy-FwowviEUW9u","expirationTime":null,"keys":{"p256dh":"BLTutiP78RubKVG_N9K9vlDLE4FBf8H4UR5MCI2o4z3vXnYDTm652AmzUS5B-juWV55XUP9qe53E9XE84FDJTHk","auth":"y1QYc_zhDHxI5bbB5aSdgw"}}
+const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/cYHtWG0LKYc:APA91bEe0TCaGynEP502gKrXmslhNnn9kxSFKjwqU4tq5QYE4_DlDNPEHdz8J2AbytdiyTodQpYxTOfeROjKPx10lRgEZsgmDAglYTDVkF6M3aiwF9XuyWmhTLuWDbn84JU3fARuE2En","expirationTime":null,"keys":{"p256dh":"BAEQ-XVfxi0lxj4ah_-zZH_fgAaTm8e6dctgd7yDXlf00PhRaXeKn78vZBloVof65WtGiU9zmDfBlbFAsgTLcy0","auth":"Rytnctvsw624PvdIlOqWVA"}}
 
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
