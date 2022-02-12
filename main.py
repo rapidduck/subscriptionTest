2baad81ed097f3d938622dbbf0c425b2d55d6fc7
rapidduck <robin.skaba@volny.cz>
2022-02-12 20:57:50 +0100

    added py way to push notifications

---
+++ main.py
@@ -0,0 +1,12 @@
+from pywebpush import webpush
+from sqlitedict import SqliteDict
+
+db = SqliteDict("db.sqlite", "w")
+
+for device, data in db.iteritems():
+    webpush(data,
+            "Hey Hey",
+            vapid_private_key="-RMX3o_klK67WLgFA2vc6IURRwqxkUQzDkHyh4bWAEw",
+            vapid_claims={"sub": "mailto:testemail@gmail.com"})
+    print(f"Pushed to {device}")
+db.close()
--- webpush.js
+++ webpush.js
@@ -13,7 +13,7 @@ webpush.setVapidDetails(
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = {"endpoint":"https://wns2-par02p.notify.windows.com/w/?token=BQYAAACNY4RzkxRpqUpNaMUtJdQUy3WBd6iz%2fL9mQdcRxeui77cSfJGRKq9xQ5dowGZbeRv5IRaH7nZnfc4dlxM9qyBPrai4%2bPduzoEkGK9IxyNhy8ae5fwzLF7BQJ9CbCnIXw11HvrkfLAWhTalpNjUBRAWHN95UcseiPUVgxwq9P%2fP2DF38B2NyNNcdFLFqigZYH%2fBotGwJVfPVVJOtI05ZomJBDg3m370eTgWkRwhLKh8rZEmGRKZQVi%2fM4SEXX3po2jJuXXk6Lvx3l9bY2M%2fzgYBKEYctLKmubh%2fi7HSh5I4OIPGsDnlEF7wM24PCKuDG3Y%2fCEB7jLzG3V8ctU3MfFxvzRIyWhDnmJRczkPvKnmGnQ%3d%3d","expirationTime":null,"keys":{"p256dh":"BOJ51sGjeqELmwLD7mspo7N5D51ygurj83L0yMa3HdRLDaAjWN9dwWEbwAo_mGV1hA3MVUmQ8MS4KQwtfCqdZzc","auth":"rO4-9PXqxLe91vyf63bscw"}}
+const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/czC7KzW_WcA:APA91bGfDY9KzPc2vsn9l7OjHiTVBIZlStA5z3OsSi_1ytOrc6AfwPZ7OGeZfxIIHbloUBy6nOeXD-Bo2go6iWnBl9Hau2-754hLxhbLC6mZkDrtSDNQsAqIrWxZ9xkUW_0SEC0OetKo","expirationTime":null,"keys":{"p256dh":"BFMxkgy6reY4kWBDnD9aDc6AU6YusFFAa1_xyCyqEnlN-SQ_IwfNQ6v-9BNSjOPH6-z1JWrkC2QfUcGarD3ObdY","auth":"nIIulDWQHZ2DzUZnl5bfiA"}}
 
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
 console.log("Performed")
